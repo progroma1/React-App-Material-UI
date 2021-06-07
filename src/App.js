48c24663114dd4bef3091f7b256f3b46822ef660
Roman Tyniv <roman.tyniv@3ss.tv>
2021-06-07 10:55:31 +0300

    finilazing small project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import {AppBar, Container, IconButton, Toolbar, Typography, Box, Paper, Grid } from '@material-ui/core';
+import {AppBar, Container, IconButton, Toolbar, Typography, Box, Paper, Grid, 
+  CardActions, Card, CardMedia, CardContent } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import LayerIcon from '@material-ui/icons/Layers';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
 
 const useStyles = makeStyles( (theme) => ({
   root: {
@@ -34,9 +36,14 @@ const useStyles = makeStyles( (theme) => ({
     position: "relative",
     padding: theme.spacing(9),
     marginTop: theme.spacing(7)
+  },
+  cardMedia: {
+    paddingTop: "56.25%"
   }
 }))
 
+
+const cards = [1,2,3,4,5,6,7,8,9];
 
 function App() {
   const classes = useStyles();
@@ -111,7 +118,7 @@ function App() {
            We are delighted to announce that Connected TV World Summit 2021 will take place as adigital event on 15th-16th June. Registration is now open for the No.1 strategy conference. We are delighted to announce that Connected TV World Summit 2021 will take place as adigital event on 15th-16th June. Registration is now open for the No.1 strategy conference
            </Typography>
            <div className={classes.mainButtons}>
-             <Grid container spacing={2} 
+             <Grid container spacing={5} 
              justify="center">
                <Grid item>
                  <Button variant="contained" color="primary">Start now</Button>
@@ -123,6 +130,39 @@ function App() {
            </div>
          </Container>
        </div>
+       <Container className={classes.cardGrid} maxWidth="md">
+        <Grid container spacing={4}>
+          {cards.map( (card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia 
+                      className={classes.cardMedia} 
+                      image="https://source.unsplash.com/random" 
+                      title="Image title"
+                    />
+                    <CardContent className={classes.CartContent}>
+                      <Typography variant="h5" gutterBottom>
+                        Blog post
+                      </Typography>
+                      <Typography>
+                        Blog post. Three Screen Solutions Three Screen Solutions Three Screen SolutionsThree Screen Solutions
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                      <LayerIcon />
+                      <PlayCircleFilledIcon />
+                    </CardActions>
+                  </Card>
+            </Grid>
+          ))}
+        </Grid>
+       </Container>
      </main>
     </>
   );
